c7d24f16f799aa61c20bbaa99223607aa758ee36
shimada337 <eisukeqoo@icloud.com>
2022-03-30 17:12:19 +0900

    マウスイベント

--- app.js
+++ app.js
@@ -20,3 +20,21 @@ $(function(){
     $('.box3').css({'background-color':'black'}).slideUp();
   });
 });
+
+$(function(){
+  $('.box4').mouseover(function(){
+    $('.box4').addClass('box4-ext');
+  });
+  $('.box4').mouseout(function(){
+    $('.box4').removeClass('box4-ext');
+  });
+});
+
+$(function(){
+  $('.box5').on('click',function(){
+    $('.box5').addClass('box5-ext');
+  });
+  $('.box5').mouseout(function(){
+  $('.box5').removeClass('box5-ext');
+});
+});
