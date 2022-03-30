89075dc9a2b95f3947334db3707782e03ca9a51b
shimada337 <eisukeqoo@icloud.com>
2022-03-30 16:46:37 +0900

    表示、非表示

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function(){
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color':'#0000ff',
+      'height':'100px'
+    }).slideUp();
+  });
+});
+
+$(function(){
+  $('.box2').slideDown(function(){
+    $('.box2').css({
+      'background-color':'black',
+    }).slideUp();
+  });
+});
+
+$(function(){
+  $('.box3').slideDown(function(){
+    $('.box3').css({'background-color':'black'}).slideUp();
+  });
 });
